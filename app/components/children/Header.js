bfb66a9508fbb5a2143dbf671cf8f3799e02f8fb
Juan <whoisju1@gmail.com>
2017-06-22 12:05:22 -0400

    beautify file

--- app/components/children/Header.js
+++ app/components/children/Header.js
@@ -1,15 +1,13 @@
-var React = require('react');
-
+var React = require("react");
 
 class Header extends React.Component {
-render(){
-return (
-<div className="jumbotron" id="header">
-<h1> Welcome to Prandium </h1>
-</div>
-
-)
-}
+  render() {
+    return (
+      <div className="jumbotron" id="header">
+        <h1> Welcome to Prandium </h1>
+      </div>
+    );
+  }
 }
 
 module.exports = Header;
