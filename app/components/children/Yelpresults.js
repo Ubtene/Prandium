d9b98ecf621c1211fb7eb3213e3147015830afa8
fodnb <Daniel.Mcghee6813@gmail.com>
2017-06-23 09:53:44 -0400

    yelp working

--- app/components/children/Yelpresults.js
+++ app/components/children/Yelpresults.js
@@ -7,7 +7,11 @@ class Yelpresults extends React.Component {
 constructor(props){
 super(props);
 this.state = {
-
+name: '',
+address: '',
+phone: '',
+link: '',
+image: ''
 }
 this.handleClick = this.handleClick.bind(this); 
 }
@@ -19,6 +23,27 @@ var zipcode = null;
 var term = null;
 console.log('clicked');
 this.props.onSubmit(zipcode, term);
+}
+
+
+
+componentDidMount(){
+
+        helpers.getYelp(this.props.zipcode, this.props.term).then(function(data){
+          console.log('victory');
+          console.log(data);
+          this.setState({
+            name: data.data.name,
+            address: data.data.location.address1,
+            phone: data.data.phone,
+            link: data.data.url,
+            image: data.data.image_url
+          })
+
+       
+
+        }.bind(this));
+
 }
 
 render(){
@@ -33,13 +58,18 @@ render(){
 
                 <div className="media-left media-top">
                   <a href="#">
-                    <img className="media-object" src="www.google.com" alt="..." width="150" height="150" />
+                    <img className="media-object" src={this.state.image} alt="..." width="150" height="150" />
                   </a>
                 </div>
 
                 <div className="media-body">
-                    <div className="well sm-well" id="reswell">Restaurant: {this.props.term}</div>
+
+                    <div className="well sm-well" id="reswell">Restaurant: {this.state.name}</div>
                     <div className="well sm-well" id="reswell">Zipcode: {this.props.zipcode}</div>
+                    <div>{this.state.address}</div>
+                    <div>{this.state.phone}</div>
+                    <div>{this.state.link}</div>
+
                 </div>
             </div>
 
