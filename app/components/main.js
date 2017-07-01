b081306bd3c1c7c7ae461007c750979616d6f93a
Juan <whoisju1@gmail.com>
2017-07-01 17:37:16 -0400

    set up main to render either of the two main sections of the site

--- app/components/main.js
+++ app/components/main.js
@@ -3,37 +3,36 @@ const Userform = require("./children/Userform");
 const Header = require("./children/Header");
 const Yelp = require("./children/Yelp");
 const FormOrLogin = require("./children/FormOrLogin");
+import CentralPage from "./children/CentralPage";
 
-{
-  /* currenlty not using the state here for user */
-}
+// use state for user info
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: false
+      user: ""
     };
 
     this.userUpdate = this.userUpdate.bind(this);
   }
 
+  // state's value becomes the user id
+  // currently do not have a way to do that 
   userUpdate(user) {
     this.setState({ user: user });
-    console.log("google updated");
+    console.log("this.state.user in <Main />", this.state.user);
   }
 
-  // Here we render the function
   render() {
     return (
       <div className="container">
         <Header />
-
         {/*This is our main component and we will need to specify what we're going to render here depending on what information is present.
-        If there is a user then setup a function to take the current user info from google and render the app else we should propably have another file that has our 
+        If there is a user then setup a function to take the current user info from google and render the app else we should probably have another file that has our 
         running app components in it.  So you will either get the first instance of our main app or the login screen..... we will need to pass the user state
         using function to run on submit to pass user info back here so we can pass it around as props for the app.
       */}
-        {this.state.user ? <Yelp /> : <FormOrLogin />}
+        {this.state.user ? <CentralPage /> : <FormOrLogin />}
       </div>
     );
   }
